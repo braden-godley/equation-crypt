85d26c91452148dcf067e11e60343b0f98d40695
Braden Godley <braden@bgodley.com>
2025-01-30 21:04:07 -0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
+use std::io::{self, BufReader, Read};
 
-fn main() -> std::io::Result<()> {
-    let f = File::open("input.txt")?;
-    let mut reader = BufReader::new(f);
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let mut reader = BufReader::new(file);
 
     let mut content = String::new();
     reader.read_to_string(&mut content)?;
@@ -12,40 +11,36 @@ fn main() -> std::io::Result<()> {
     let my_equation = |x| (x as f32).cos() * 10.0;
 
     let encrypted = encrypt(&content, my_equation)?;
-
-    println!("Encrypted: {}", encrypted);
+    println!("Encrypted:\n{}\n", encrypted);
 
     let decrypted = decrypt(&encrypted, my_equation)?;
-
-    println!("Decrypted: {}", decrypted);
+    println!("Decrypted:\n{}\n", decrypted);
 
     Ok(())
 }
 
 type Equation = fn(i32) -> f32;
 
-fn algorithm(input: &str, equation: Equation, mult: i32) -> std::io::Result<String> {
-    let mut out = String::new();
+fn algorithm(input: &str, equation: Equation, mult: i32) -> io::Result<String> {
+    let mut output = String::new();
 
-    let mut j = 0;
-    for c in input.chars().enumerate() {
-        let byte = c.1 as u8;
-        let offset: i32 = equation(j).floor() as i32 * mult;
-        let total = byte as i32 + offset as i32;
+    for (j, c) in input.chars().enumerate() {
+        let byte = c as u8;
+        let offset: i32 = equation(j.try_into().unwrap()).floor() as i32 * mult;
+        let total = byte as i32 + offset;
         let result = (total % 256) as u8;
-        println!("{} + {} -> {} -> {}", byte, offset, total, result);
-        out.push(result as char);
-        j += 1;
+
+        //println!("{} + {} -> {} -> {}", byte, offset, total, result);
+        output.push(result as char);
     }
 
-    Ok(out)
+    Ok(output)
 }
 
-fn encrypt(input: &str, equation: Equation) -> std::io::Result<String> {
+fn encrypt(input: &str, equation: Equation) -> io::Result<String> {
     algorithm(input, equation, 1)
 }
 
-fn decrypt(input: &str, equation: Equation) -> std::io::Result<String> {
+fn decrypt(input: &str, equation: Equation) -> io::Result<String> {
     algorithm(input, equation, -1)
 }
-
